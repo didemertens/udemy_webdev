a14066cda54eb08ce5f135ef48782ec80c374da1
Dide Mertens <d.g.j.mertens@gmail.com>
2019-03-09 11:12:30 +0000

    JS syntax while loops

---
+++ JavaScript/js_syntax_while.js
@@ -0,0 +1,22 @@
+// var x = 0;
+
+// while (x < 5){
+//   console.log("x is currently: "+x);
+
+//   if (x === 3){
+//     console.log("x is equal to THREE!")
+//     break;
+//   }
+
+//   console.log("x is still less than 5, adding 1 to x");
+//   x = x+1;
+// }
+
+var x = 1;
+
+while (x < 11 ) {
+  if (x % 2 === 0){
+    console.log(x);
+  }
+  x = x+1;
+}
