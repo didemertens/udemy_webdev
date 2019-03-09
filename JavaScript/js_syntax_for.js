807e210e2da4e9b55a10ccb56f09a7c2f373619a
Dide Mertens <d.g.j.mertens@gmail.com>
2019-03-09 11:20:35 +0000

    JS syntax for loops

---
+++ JavaScript/js_syntax_for.js
@@ -0,0 +1,15 @@
+// for (var i = 0; i<5; i++){
+//   console.log(i)
+// }
+
+// var word = "abcdefghijk"
+
+// for (var i = 0; i < word.length; i++) {
+//   console.log(word[i])
+// }
+
+var word = "ababababab"
+
+for (var i = 0; i < word.length; i+=2) {
+  console.log(word[i])
+}
