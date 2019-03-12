4254708cde49db0cbc6d985b08bb6163ac7d5021
Dide Mertens <d.g.j.mertens@gmail.com>
2019-03-12 20:35:01 +0000

    OOP classes/child classes basics

---
+++ Python/oop_extra_prac.py
@@ -0,0 +1,47 @@
+class Dog:
+
+    # Class attribute
+    species = 'mammal'
+
+    # Initializer / Instance attributes
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # Instance method
+    def description(self):
+        return self.name, self.age
+
+    # Instance method
+    def speak(self, sound):
+        return "%s says %s" % (self.name, sound)
+
+    # Instance method
+    def eat(self):
+        self.is_hungry = False
+
+
+# Child class (inherits from Dog class)
+class RussellTerrier(Dog):
+    def run(self, speed):
+        return "%s runs %s" % (self.name, speed)
+
+
+# Child class (inherits from Dog class)
+class Bulldog(Dog):
+    def run(self, speed):
+        return "%s runs %s" % (self.name, speed)
+
+sam = Dog("Sam",9)
+bobby = Dog("Bobby",2)
+# nora = Dog("Nora",4)
+
+def get_biggest_number(*args):
+  return max(args)
+
+oldest = get_biggest_number(sam.age,bobby.age,nora.age)
+print(f"The oldest dog is {oldest} years old.")
+
+
+
+
