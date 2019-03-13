7e7d4f6fbaafe3998991fac8a19882ffe9e8917c
Dide Mertens <d.g.j.mertens@gmail.com>
2019-03-13 21:21:10 +0000

    OOP added management system to test all different methods

--- Python/oop_farm.py
+++ Python/oop_farm.py
@@ -38,8 +38,8 @@ class Crop:
     self._update_status()
 
 
-def auto_grow(crop,days):
-  for i in range(days):
+def auto_grow(crop):
+  for i in range(30):
     light = random.randint(1,10)
     water = random.randint(1,10)
     crop.grow(light,water)
@@ -66,17 +66,57 @@ def manual_grow(crop):
         print("Value not valid - please try again.")
     except ValueError:
       print("Value not valid - please try again.")
+  # Grow the crop
   crop.grow(light,water)
+
+def display_menu():
+  print("1. Grow manually over 1 day")
+  print("2. Grow automatically over 30 days")
+  print("3. Report status")
+  print("0. Exit test program")
+  print()
+  print("Please select an option from the above menu.")
+  print()
+
+def get_choice_menu():
+  option_valid = False
+  while not option_valid:
+    try:
+      choice = int(input("Option selected: "))
+      if 0 <= choice <= 4:
+        option_valid = True
+      else:
+        print("Please enter a valid option.")
+    except ValueError:
+      print("Please enter a valid option.")
+  return choice
+
+def manage_crop(crop):
+  print("This is the crop management program.")
+  print()
+  noexit = True
+  while noexit:
+    display_menu()
+    option = get_choice_menu()
+    if option == 1:
+      manual_grow(crop)
+      print()
+    elif option == 2:
+      auto_grow(crop)
+      print()
+    elif option == 3:
+      print(crop.report())
+      print()
+    elif option == 0:
+      noexit = False
+  print("See you later!")
 
 def main():
   new_crop = Crop(1,4,3)
   # print(new_crop.needs()['light need'])
   # print(new_crop.report())
   # new_crop.grow(5,5)
-  manual_grow(new_crop)
-  print(new_crop.report())
-
-
+  manage_crop(new_crop)
 
 
 if __name__ == "__main__":
