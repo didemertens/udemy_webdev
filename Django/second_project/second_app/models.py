2ffc6e3d809a5b03025b0f5e02bea49c2033ced1
Dide Mertens <d.g.j.mertens@gmail.com>
2019-03-23 14:02:40 +0000

    Added user module to second project,

--- Django/second_project/second_app/models.py
+++ Django/second_project/second_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  first_name = models.CharField(max_length=263,unique=True)
+  last_name = models.CharField(max_length=263,unique=True)
+  email = models.EmailField(max_length=263,unique=True)
+
+  def __str__(self):
+    return self.first_name + ' ' + self.last_name + ': ' + self.email
