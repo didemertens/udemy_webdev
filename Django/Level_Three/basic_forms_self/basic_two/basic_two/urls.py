ae8215f9beb1440b5f2a32752c85720d9d81bd37
Dide Mertens <d.g.j.mertens@gmail.com>
2019-03-23 18:17:10 +0000

    added html file for redirect after form is submitted

--- Django/Level_Three/basic_forms_self/basic_two/basic_two/urls.py
+++ Django/Level_Three/basic_forms_self/basic_two/basic_two/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index),
     path('form/',views.form_page),
+    path('thanks/',views.thanks),
 ]
--- Django/Level_Three/basic_forms_self/basic_two/basicapp/views.py
+++ Django/Level_Three/basic_forms_self/basic_two/basicapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from basicapp import forms
 
 # Create your views here.
@@ -12,7 +12,11 @@ def form_page(request):
     form = forms.NameForm(request.POST)
 
   if form.is_valid():
-    return HttpResponse('Thanks '+ form.cleaned_data['name'] + '!')
+    # return HttpResponse('Thanks '+ form.cleaned_data['name'] + '!')
+    return HttpResponseRedirect('thanks/')
   else:
     form = forms.NameForm()
   return render(request,'basicapp/nameform.html',{'form':form})
+
+def thanks(request):
+  return render(request, 'basicapp/thanks.html')
