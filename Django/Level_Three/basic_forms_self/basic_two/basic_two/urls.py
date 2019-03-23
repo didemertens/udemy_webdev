567f81507c30f851da06bbf40f925e96448d53db
Dide Mertens <d.g.j.mertens@gmail.com>
2019-03-23 17:06:28 +0000

    Made basic form in django project

--- Django/Level_Three/basic_forms_self/basic_two/basic_two/urls.py
+++ Django/Level_Three/basic_forms_self/basic_two/basic_two/urls.py
@@ -20,4 +20,5 @@ from basicapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index),
+    path('form/',views.form_page),
 ]
--- Django/Level_Three/basic_forms_self/basic_two/basicapp/views.py
+++ Django/Level_Three/basic_forms_self/basic_two/basicapp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from basicapp import forms
 
 # Create your views here.
 def index(request):
   return render(request,'basicapp/index.html')
+
+def form_page(request):
+  form = forms.NameForm()
+  if request.method == 'POST':
+    form = forms.NameForm(request.POST)
+
+  if form.is_valid():
+    return HttpResponse('Thanks '+ form.cleaned_data['name'] + '!')
+  else:
+    form = forms.NameForm()
+  return render(request,'basicapp/nameform.html',{'form':form})
