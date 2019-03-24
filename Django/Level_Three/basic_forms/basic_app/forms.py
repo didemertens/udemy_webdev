550e1107c7a65456be996b2ac5083062a4122b40
Dide Mertens <d.g.j.mertens@gmail.com>
2019-03-24 17:15:48 +0000

    added django validators to catch bots

--- Django/Level_Three/basic_forms/basic_app/forms.py
+++ Django/Level_Three/basic_forms/basic_app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core import validators
 
 
 class FormName(forms.Form):
@@ -6,10 +7,13 @@ class FormName(forms.Form):
   email = forms.EmailField()
   text = forms.CharField(widget=forms.Textarea)
   botcatcher = forms.CharField(required=False,
-                              widget=forms.HiddenInput)
+                              widget=forms.HiddenInput,
+                              validators=[validators.MaxLengthValidator(0)])
 
-  def clean_botcatcher(self):
-    botcatcher = self.cleaned_data['botcatcher']
-    if len(botcatcher) > 0:
-      raise forms.ValidationError("Gotcha")
-    return botcatcher
+  # def clean_botcatcher(self):
+  #   botcatcher = self.cleaned_data['botcatcher']
+  #   if len(botcatcher) > 0:
+  #     raise forms.ValidationError("Gotcha")
+  #   return botcatcher
+
+
