bcaaf2872c0303a0756cb416cfb472e9bca505fc
Dide Mertens <d.g.j.mertens@gmail.com>
2019-03-24 20:07:27 +0000

    Trying to add email validation to modelform

--- Django/Level_Three/test_model_forms/model_forms/basicapp/forms.py
+++ Django/Level_Three/test_model_forms/model_forms/basicapp/forms.py
@@ -2,6 +2,21 @@ from django import forms
 from basicapp.models import User
 
 class NewUserForm(forms.ModelForm):
+  # message = forms.CharField(max_length=1000, widget=forms.Textarea,label='Write a message')
+  verify_email = forms.EmailField(label='Enter your email again')
+
   class Meta:
     model = User
-    fields = '__all__'
+    fields = ['name','email','verify_email']
+
+    # def clean(self):
+    #   all_clean_data = super().clean()
+    #   email = all_clean_data['email']
+    #   vmail = all_clean_data['verify_email']
+    #   if email != vmail:
+    #     raise forms.ValidationError("Make sure emails match")
+    #   else:
+    #     return email
+
+
+
