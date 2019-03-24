ddeaf7275e21974b3923543d4185ec530c876722
Dide Mertens <d.g.j.mertens@gmail.com>
2019-03-24 17:18:20 +0000

    Added own validator (function with value as parameter)

--- Django/Level_Three/basic_forms/basic_app/forms.py
+++ Django/Level_Three/basic_forms/basic_app/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 from django.core import validators
 
+# because of value, django knows it's a validator
+def check_for_z(value):
+  if value[0].lower() != 'z':
+    raise forms.ValidationError("Needs to start with z")
 
 class FormName(forms.Form):
-  name = forms.CharField()
+  name = forms.CharField(validators=[check_for_z])
   email = forms.EmailField()
   text = forms.CharField(widget=forms.Textarea)
   botcatcher = forms.CharField(required=False,
