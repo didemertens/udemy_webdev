7d3a2cf123388703b5ba3ab19f09f88f094836a2
Dide Mertens <d.g.j.mertens@gmail.com>
2019-03-25 20:54:14 +0000

    Added template tags for own html page, admin and index

--- Django/Level_Four/template_project/basic_app/urls.py
+++ Django/Level_Four/template_project/basic_app/urls.py
@@ -3,3 +3,8 @@ from basic_app import views
 
 # Template tagging
 app_name = 'basic_app'
+
+urlpatterns = [
+  path('relative/',views.relative,name='relative'),
+  path('other/',views.other,name='other'),
+]
