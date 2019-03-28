76e99242c8390e7ad8ee90d7c051e2ca66f8ee36
Dide Mertens <d.g.j.mertens@gmail.com>
2019-03-28 20:32:02 +0000

    Added views to create register form for users

--- Django/Level_Five/learning_users/basic_app/forms.py
+++ Django/Level_Five/learning_users/basic_app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from basic_app import UserProfileInfo
+from basic_app.models import UserProfileInfo
 
 class UserForm(forms.ModelForm):
   password = forms.CharField(widget=forms.PasswordInput())
--- Django/Level_Five/learning_users/learning_users/urls.py
+++ Django/Level_Five/learning_users/learning_users/urls.py
@@ -19,6 +19,6 @@ from basic_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name=index),
+    path('', views.index, name='index'),
     path('basic_app/', include('basic_app.urls')),
 ]
