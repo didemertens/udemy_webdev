c3e85226a7dfcb06aec6757d08e7afb49d13076b
Dide Mertens <d.g.j.mertens@gmail.com>
2019-03-29 20:41:47 +0000

    added folder for deployment

--- Django/Level_Four/template_project/basic_app/templatetags/my_extras.py
+++ Django/Level_Four/template_project/basic_app/templatetags/my_extras.py
@@ -2,10 +2,12 @@ from django import template
 
 register = template.Library()
 
+@register.filter(name='my_cut')
 def my_cut(value,arg):
   """
   this cuts out all values of 'arg' from the string
   """
   return value.replace(arg,'')
 
-register.filter('my_cut',my_cut)
+# register.filter('my_cut',my_cut)
+# can also use decorators, @register.filter...
