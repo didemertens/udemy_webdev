30f9b02123e9214b4c6f6d760aa5e11d232af2b8
Dide Mertens <d.g.j.mertens@gmail.com>
2019-03-30 16:10:31 +0000

    TemplateView with context dict

--- Django/advanced/cbv_project/basic_app/views.py
+++ Django/advanced/cbv_project/basic_app/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, TemplateView
 from django.http import HttpResponse
 
 # # Create your views here.
 # def index(request):
 #   return render(request,'index.html')
 
-class CBView(View):
+# class CBView(View):
 
-  def get(self,request):
-    return HttpResponse('Class based views are cool!')
+#   def get(self,request):
+#     return HttpResponse('Class based views are cool!')
+
+class IndexView(TemplateView):
+  template_name = 'index.html'
+
+  def get_context_data(self,**kwargs):
+    # kwargs gives a dictionary, args is for undefinied amount of arguments
+    context = super().get_context_data(**kwargs)
+    context['injectme'] = 'Basic injection!'
+    return context
+
