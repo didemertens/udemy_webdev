1217c56fd50c2018b70a1c885c431f338ba53097
Dide Mertens <d.g.j.mertens@gmail.com>
2019-03-31 13:50:57 +0100

    added views for post class

--- Django/first_clone/mysite/blog/forms.py
+++ Django/first_clone/mysite/blog/forms.py
@@ -3,24 +3,26 @@ from blog.models import Post,Comment
 
 class PostForm(forms.ModelForm):
 
-  class Meta():
+  class Meta:
     model = Post
     fields = ('author','title','text')
 
     widgets = {
-      'title':forms.TextInput(attrs={'class':'textinputclass'}),
-      'text':forms.TextArea(attrs={'class':'editable medium-editor-textarea postcontent'}),
+      'title' : forms.TextInput(attrs={'class': 'textinputclass'}),
+      'text' : forms.Textarea(attrs={'class': 'editable medium-editor-textarea postcontent'}),
     }
 
 
 class CommentForm(forms.ModelForm):
 
-  class Meta():
+  class Meta:
     model = Comment
     fields = ('author','text')
 
 
     widgets = {
-      'author':forms.TextInput(attrs={'class':'textinputclass'}),
+      'author' : forms.TextInput(attrs={'class':'textinputclass'}),
       'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea'}),
     }
+
+
