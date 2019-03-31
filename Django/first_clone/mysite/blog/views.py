213a732233d9eb4031d485faa0e03fc6cc18912e
Dide Mertens <d.g.j.mertens@gmail.com>
2019-03-31 16:03:25 +0100

    made simple blog, adding/deleting posts and comments

--- Django/first_clone/mysite/blog/views.py
+++ Django/first_clone/mysite/blog/views.py
@@ -54,7 +54,7 @@ class DraftListView(LoginRequiredMixin,ListView):
 @login_required
 def post_publish(request,pk):
   post = get_object_or_404(Post,pk=pk)
-  post.publish
+  post.publish()
   return redirect('post_detail',pk=pk)
 
 """
