f6c171575da5f10856c248371337d1b72bc85b48
Dide Mertens <d.g.j.mertens@gmail.com>
2019-03-31 13:19:43 +0100

    added forms (modelform)

--- Django/first_clone/mysite/blog/forms.py
+++ Django/first_clone/mysite/blog/forms.py
@@ -1,1 +1,26 @@
-forms.py
+from django import forms
+from blog.models import Post,Comment
+
+class PostForm(forms.ModelForm):
+
+  class Meta():
+    model = Post
+    fields = ('author','title','text')
+
+    widgets = {
+      'title':forms.TextInput(attrs={'class':'textinputclass'}),
+      'text':forms.TextArea(attrs={'class':'editable medium-editor-textarea postcontent'}),
+    }
+
+
+class CommentForm(forms.ModelForm):
+
+  class Meta():
+    model = Comment
+    fields = ('author','text')
+
+
+    widgets = {
+      'author':forms.TextInput(attrs={'class':'textinputclass'}),
+      'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea'}),
+    }
