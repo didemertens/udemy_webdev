b5be9154fd95fb0e495988c5ce5cbf1944c51af6
Dide Mertens <d.g.j.mertens@gmail.com>
2019-04-07 14:35:12 +0100

    styled forms for comments and new/edit posts

--- Django/advanced/test_Blog/test_bookblog/book_app/forms.py
+++ Django/advanced/test_Blog/test_bookblog/book_app/forms.py
@@ -7,9 +7,20 @@ class PostForm(forms.ModelForm):
     model = Blog
     fields = ('title', 'body')
 
+    widgets = {
+    'title': forms.TextInput(attrs={'class': 'post-title'}),
+    'body': forms.Textarea(attrs={'class': 'post-content'}),
+    }
+
 class CommentForm(forms.ModelForm):
 
   class Meta:
     model = Comment
     fields = ('author', 'text')
+
+    widgets = {
+    'author': forms.TextInput(attrs={'class': 'author-comment'}),
+    'text': forms.Textarea(attrs={'class': 'text-comment'}),
+    }
+
 
