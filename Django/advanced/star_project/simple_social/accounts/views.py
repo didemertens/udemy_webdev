8ef1c7105962021f81dca0efa2e0bdb08b094410
Dide Mertens <d.g.j.mertens@gmail.com>
2019-04-11 20:57:16 +0100

    connected login and logout pages

--- Django/advanced/star_project/simple_social/accounts/views.py
+++ Django/advanced/star_project/simple_social/accounts/views.py
@@ -4,6 +4,7 @@ from django.views.generic import CreateView
 
 from . import forms
 # Create your views here.
+
 class SignUp(CreateView):
   form_class = forms.UserCreateForm
   success_url = reverse_lazy('login')
