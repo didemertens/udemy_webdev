602466bd65e94c76c4a5c1d266bc1f02922b4634
Dide Mertens <d.g.j.mertens@gmail.com>
2019-04-13 16:43:03 +0100

    changed field order on admin page

--- Django/advanced/change_admin/app_videos/admin.py
+++ Django/advanced/change_admin/app_videos/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+
+  fields = ['release_year','title','length']
+
 admin.site.register(models.Customer)
-admin.site.register(models.Movie)
+admin.site.register(models.Movie,MovieAdmin)
